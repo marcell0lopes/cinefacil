7a559b1d99ab56ba8a311c4c0e213e56f7346325
Marcello Lopes <marcell0lopes@outlook.com>
2023-02-05 01:25:33 -0300

    initial config

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,29 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="pt-br">
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="description"
+          content="Descubra o próximo filme que vai te emocionar ou a próxima série para maratonar"
+        />
+        <meta
+          property="og:description"
+          content="Descubra o próximo filme que vai te emocionar ou a próxima série para maratonar"
+        />
+        <meta property="og:title" content="CineFacil" />
+        <meta name="twitter:title" content="CineFacil" />
+        <meta
+          name="twitter:description"
+          content="Descubra o próximo filme que vai te emocionar ou a próxima série para maratonar"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
