ef83ea9e3bd513f86b4710c00b7a0248ad3132bb
Marcello Lopes <marcell0lopes@outlook.com>
2023-02-05 01:25:51 -0300

    openapi stream route

---
+++ pages/api/generate.ts
@@ -0,0 +1,40 @@
+import { OpenAIStream, OpenAIStreamPayload } from "@/utils/OpenAIStream";
+
+if (!process.env.OPENAI_API_KEY) {
+  throw new Error(
+    "Não foi encontrada a variável de ambiente relacionada a chave de API OpenAI"
+  );
+}
+
+export const config = {
+  runtime: "edge",
+};
+
+const handler = async (req: Request): Promise<Response> => {
+  const { prompt } = (await req.json()) as {
+    prompt?: string;
+  };
+
+  if (!prompt) {
+    return new Response("Não foi enviado um prompt no request", {
+      status: 400,
+    });
+  }
+
+  const payload: OpenAIStreamPayload = {
+    model: "text-davinci-003",
+    prompt,
+    temperature: 0.7,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+    max_tokens: 200,
+    stream: true,
+    n: 1,
+  };
+
+  const stream = await OpenAIStream(payload);
+  return new Response(stream);
+};
+
+export default handler;
---
+++ utils/OpenAIStream.ts
@@ -0,0 +1,85 @@
+import {
+  createParser,
+  ParsedEvent,
+  ReconnectInterval,
+} from "eventsource-parser";
+
+export type OpenAIStreamPayload = {
+  model: string;
+  prompt: string;
+  temperature: number;
+  top_p: number;
+  frequency_penalty: number;
+  presence_penalty: number;
+  max_tokens: number;
+  stream: boolean;
+  n: number;
+};
+
+export async function OpenAIStream(payload: OpenAIStreamPayload) {
+  const encoder = new TextEncoder();
+  const decoder = new TextDecoder();
+
+  let counter = 0;
+
+  const res = await fetch("https://api.openai.com/v1/completions", {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${process.env.OPENAI_API_KEY ?? ""}`,
+    },
+    method: "POST",
+    body: JSON.stringify(payload),
+  });
+
+  const stream = new ReadableStream({
+    async start(controller) {
+      //callback
+      function onParse(event: ParsedEvent | ReconnectInterval) {
+        if (event.type === "event") {
+          const data = event.data;
+
+          console.group("event");
+          console.log("Received event!");
+          console.log("id: %s", event.id || "<none>");
+          console.log("data: %s", event.data);
+          console.groupEnd();
+
+          if (data === "[DONE]") {
+            controller.close();
+            return;
+          }
+
+          try {
+            const json = JSON.parse(data);
+            const text = json.choices[0].text;
+            if (counter < 2 && (text.match(/\n/) || []).length) {
+              // esse caractere é um prefixo (ex: "\n\n"), não faz nada.
+              return;
+            }
+            const queue = encoder.encode(text);
+            controller.enqueue(queue);
+            counter++;
+          } catch (e) {
+            controller.error(e);
+          }
+        } else if (event.type === "reconnect-interval") {
+          console.log(
+            "We should set reconnect interval to %d milliseconds",
+            event.value
+          );
+        }
+      }
+
+      // A response da stream (SSE) da OpenAi pode ser fragmentada em vários 'chunks'
+      // isso garante que nós vamos ler esses chunks de maneira apropriada e vamos invocar um evento para cada stream SSE
+
+      const parser = createParser(onParse);
+      // https://web.dev/streams/#asynchronous-iteration
+      for await (const chunk of res.body as any) {
+        parser.feed(decoder.decode(chunk));
+      }
+    },
+  });
+
+  return stream;
+}
